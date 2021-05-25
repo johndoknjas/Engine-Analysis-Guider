5edbe46e632feb65aaf5583b66f2bf7c675fd152
John Doknjas <jdoknjas@sfu.ca>
2021-05-24 22:20:05 -0700

    Specify depth, #principle variations, #threads, and display outputs.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from stockfish import Stockfish
 
-stockfish = Stockfish(r"C:\Users\johnd\Documents\Fun Coding Projects\Stockfish Guider\stockfish-10-win\Windows\stockfish_10_x64.exe")
+stockfish = Stockfish(path = r"C:\Users\johnd\Documents\Fun Coding Projects\Stockfish Guider\stockfish-10-win\Windows\stockfish_10_x64.exe",
+                      depth = 28, parameters = {"MultiPV": 1, "Threads": 4})
 
 def main():
     print("Hi")
+    #stockfish.set_fen_position("4rrk1/pp1nqpbp/3p2p1/2nPP3/2p2P2/P1N1BQPP/1PB3K1/3R1R2 b - - 0 20")
     print(stockfish.get_evaluation())
     print(stockfish.get_best_move())
     
