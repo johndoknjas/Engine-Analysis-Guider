29d4e4c93e4f502ecbcbae2cf0272264e702c585
John Doknjas <jdoknjas@sfu.ca>
2021-05-27 23:32:52 -0700

    Some more ideas

--- main.py
+++ main.py
@@ -27,6 +27,33 @@
 # the likely best moves to continue on with being guided through the search tree. So having
 # a completely accurate evaluation isn't as important as it is in a final/leaf position
 # of the search tree, since that evaluation is used for minimax.
+    
+# If a move from the opponent (somewhere in the search tree) gives the goal evaluation, or
+# at least a fair amount better than it, no need to continue searching there. The app can just
+# print the evlauation, and then I can check it out myself in my own analysis. The main thing for
+# the app to do is analyse lines that seriously challenge the goal evaluation.
+    
+# The user can specify the normal depth to search to, and the depth to search to when at a leaf node.
+    
+# Use an SQL database to store engine evaluations in positions. This can allow the app to
+# stop running at some point, and then run again later on (the search and evaluation of those
+# positions will be near instant, since it will just use the DB).
+    
+# The user can also input the threshold to not analyse a move if it's below the goal evaluation
+# by some amount.
+    
+# Since the app mainly looks at moves from the player that is not dramatically above the
+# goal evaluation, and movesf from the opponent that aren't at the evlauation or within it,
+# the branching factor shouldn't be too bad? The app doesn't need to definitvely prove that a
+# line gives the goal evaluation. It should analyse similar to how you do - looking at the main lines,
+# not analysing a line that is most likely irrelevant.
+    
+# If the app reaches the gaol evaluation at some point in the search, decide whether it should stop there
+# or continue searching further (to ensure the gaol evaluation holds). This could be a parameter
+# the user also specifies.
+    
+# If the app can't find a line giving the gaol evaluation, it should give line(s) that is the
+# best it can do. Still may be useful.
 
 from stockfish import Stockfish
 
